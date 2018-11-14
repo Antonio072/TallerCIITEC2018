a746aaedb040881b7ed82caaaee712ee4e5c8cf3
JorgeDelgadillo <jorgdelgadillo@gmail.com>
2018-11-14 12:17:48 -0600

    Se cambio for a foreach
    😄

--- TallerCIITEC.java
+++ TallerCIITEC.java
@@ -9,8 +9,8 @@ public class TallerCIITEC {
 
     public static void main(String[] args) {
       int[]variable={1,2,3,4,5,6,7};
-      for (int i=0;i<=6;i++){
-          System.out.println("numero" + variable[i]);
+      foreach(int i : variable){
+        System.out.println(i+" ");
       }
     }
 }
