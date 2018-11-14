e46b7932e755686f404eea53c3952ead9e2145a9
Antonio Velazquez <31423917+Antonio072@users.noreply.github.com>
2018-11-14 09:07:59 -0600

    Agregada la clase de Java
    Con esta clase se trabajará para que se vaya modificando a lo largo del taller

---
+++ TallerCIITEC.java
@@ -0,0 +1,35 @@
+
+package tallerciitec;
+/**
+ *
+ * @author antonio072
+ * Clase de Java creada para el taller de Git y Github
+ */
+public class TallerCIITEC {
+
+    public static void main(String[] args) {
+      int a;
+      int b;
+      int c;
+      int d;
+      int e;
+      int f;
+      int g;
+      a=1;
+      b=2;
+      c=3;
+      d=4;
+      e=5;
+      f=6;
+      g=7;
+      
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+    }
+    
+}
