6be0b2ce6d1b0a24756ccfa3f7b96f33626d32e9
JorgeDelgadillo <jorgdelgadillo@gmail.com>
2018-11-14 12:09:11 -0600

    Se cambio la estructura
    Se creo un array con los numeros y se imprimen con un for

--- TallerCIITEC.java
+++ TallerCIITEC.java
@@ -8,28 +8,12 @@ package tallerciitec;
 public class TallerCIITEC {
 
     public static void main(String[] args) {
-      int a;
-      int b;
-      int c;
-      int d;
-      int e;
-      int f;
-      int g;
-      a=1;
-      b=2;
-      c=3;
-      d=4;
-      e=5;
-      f=6;
-      g=7;
+      int[] numeros = {1, 2, 3, 4, 5, 6, 7};
       
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
-        System.out.println(d);
-        System.out.println(e);
-        System.out.println(f);
-        System.out.println(g);
+        
+        for(int i = 0 ; i < numeros.length; i++){
+            System.out.println(numeros[i]);
+        }
     }
     
 }
