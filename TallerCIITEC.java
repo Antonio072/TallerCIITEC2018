ffac984dfe6af7637e21a23f6ee41e4c9b0882a6
Antonio Velazquez <31423917+Antonio072@users.noreply.github.com>
2018-11-14 12:37:22 -0600

    Esto fixes #19
    Esto arregla el problema #19

--- TallerCIITEC.java
+++ TallerCIITEC.java
@@ -6,7 +6,7 @@ package tallerciitec;
  * Clase de Java creada para el taller de Git y Github
  */
 public class TallerCIITEC {
-
+    System.exit(1);
     public static void main(String[] args) {
       int[]variable={1,2,3,4,5,6,7};
       foreach(int i : variable){
