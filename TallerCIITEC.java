f05db2f468efa0a58de51aa03a67777f7b8c5b5a
EduardoGuerreroCa <45045163+EduardoGuerreroCa@users.noreply.github.com>
2018-11-14 12:08:55 -0600

    Cambie el tipo de valor de las variables
    Para realizar una suma con todas las variables declaradas

--- TallerCIITEC.java
+++ TallerCIITEC.java
@@ -8,28 +8,11 @@ package tallerciitec;
 public class TallerCIITEC {
 
     public static void main(String[] args) {
-      int a;
-      int b;
-      int c;
-      int d;
-      int e;
-      int f;
-      int g;
-      a=1;
-      b=2;
-      c=3;
-      d=4;
-      e=5;
-      f=6;
-      g=7;
+      double a=1,b=2,c=3,d=4,e=5,f=6,g=7,r;
       
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
-        System.out.println(d);
-        System.out.println(e);
-        System.out.println(f);
-        System.out.println(g);
+        r=(a+b+c+d+e+f+g);
+      
+        System.out.println(r);
     }
     
 }
